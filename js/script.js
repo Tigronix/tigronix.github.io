6a1afcb9a6bee38a8b8c85478a6bbb331b9e779a
vitold <vditold@yandex.ru>
2016-08-29 16:20:21 +0300

    123123

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
     $('#product-name').val(title + pathname);
     
     //Для прилипания над футером
-    var fixed = $('.fixed-menu').scroll;
+    /*var fixed = $('.fixed-menu').offset().top;
     var Elem = $('.footer').offset().top;
     var footer = $('.footer').innerHeight();
     console.log(fixed);
@@ -35,7 +35,30 @@ $(document).ready(function(){
         else {
             $('.fixed-menu').removeClass('unfixed');
         }
-    });
+    });*/
+    	$(document).ready(function() {
+		goTop();
+		$(document).on('click', '.go-top', function(){
+			$('body,html').animate({scrollTop: 0}, 500);
+		});
+	});
+	function goTop(){
+		var scroll = $('.fixed-menu').offset().top;
+        var footer = $('.footer').offset().top;
+        var footer_size = $('.footer').innerHeight();
+        var true_class = $('.go-top').hasClass('go-top--show');
+        console.log(true_class);
+		
+		if (scroll >= footer){
+			$('.go-top').addClass('go-top--show');
+			
+		}else{
+			$('.go-top').removeClass('go-top--show');
+		}
+	}
+    window.onscroll = function() {
+		goTop();
+	}
     
     //Мобильное меню
     $('#mob-menu').click(function(){
