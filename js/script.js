6eaf93cc4f5ae02bb1e781057aebedf7c070c777
vitold <vditold@yandex.ru>
2016-09-02 11:58:07 +0300

    иллюзион

--- js/script.js
+++ js/script.js
@@ -19,5 +19,42 @@ $(document).ready(function(){
             $('.menu .fa').hide();
             $('.menu .phone').hide();
         }
-    });  
+    }); 
+    //mob-menu
+        $('#mob-menu').click(function(){
+           $(this).hide();
+           $('#mob-close').show();
+           $('.mob-switch').fadeIn(300);
+        });
+        $('#mob-close').click(function(){
+           $(this).hide();
+           $('.mob-switch').fadeOut(300);
+           $('#mob-menu').show();
+    }); 
+    //slider
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      fade: true,
+      asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+      slidesToShow: 10,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      focusOnSelect: true
+    });    
+
+    //hover в слайдере
+    $('.slick-prev').hover(function(){
+       if ($(this).hover) {
+           $('.fa-angle-left').toggleClass('hover');
+       }
+    });
+    $('.slick-next').hover(function(){
+       if ($(this).hover) {
+           $('.fa-angle-right').toggleClass('hover');
+       }
+    });
 });
