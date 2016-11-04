722b44707fff1f8ac5e1695cc127607e5e0638e6
vitold <vditold@yandex.ru>
2016-11-04 15:07:29 +0300

    123

--- js/script.js
+++ js/script.js
@@ -16,11 +16,13 @@ $(document).ready(function () {
 	//mob-menu
 	$('.header__open').click(function () {
 		$(this).fadeOut(300);
-		$('.header__close').fadeIn(300).addClass('ib');	$('.header__nav').addClass('header__on').removeClass('move_away').addClass('move_left').fadeIn(1200);
+		$('.header__close').fadeIn(300).addClass('ib');	$('.header__nav').addClass('header__on').removeClass('move_away').addClass('move_left_header').fadeIn(400);
+		$('.blackout').fadeIn(400);
 	});
 	$('.header__close').click(function () {
-		$(this).fadeOut(300);	$('.header__nav').removeClass('move_left').addClass('move_away').fadeOut(1200);
+		$(this).fadeOut(300);	$('.header__nav').removeClass('move_left_header').addClass('move_away').fadeOut(400);
 		$('.header__open').fadeIn(300).addClass('ib');
+		$('.blackout').fadeOut(400);
 	});
 	
 
