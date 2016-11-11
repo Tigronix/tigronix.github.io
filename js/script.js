1bc9887c2ad89b3c868d00e7866af00c871fbc6f
vitold <vditold@yandex.ru>
2016-11-11 14:02:12 +0300

    123

--- js/script.js
+++ js/script.js
@@ -1,3 +1,122 @@
+
+$(document).ready(function() {
+
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+
+		/* Check the location of each desired element */
+		$('.hideme').each( function(i){
+
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			/* If the object is completely visible in the window, fade it it */
+			if( bottom_of_window > bottom_of_object ){
+
+				$(this).animate({'opacity':'1'},1500);
+
+			}
+
+		}); 
+
+	});
+
+});
+
+$(document).ready(function() {
+
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+
+		/* Check the location of each desired element */
+		$('.slide_to_left').each( function(i){
+
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			/* If the object is completely visible in the window, fade it it */
+			if( bottom_of_window > bottom_of_object ){
+
+				$(this).addClass('slide_left')
+
+			}
+
+		}); 
+
+	});
+
+});
+
+$(document).ready(function() {
+
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+
+		/* Check the location of each desired element */
+		$('.slide_to_right').each( function(i){
+
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			/* If the object is completely visible in the window, fade it it */
+			if( bottom_of_window > bottom_of_object ){
+
+				$(this).addClass('slide_right')
+
+			}
+
+		}); 
+
+	});
+
+});
+
+$(document).ready(function() {
+
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+
+		/* Check the location of each desired element */
+		$('.slide_to_top').each( function(i){
+
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			/* If the object is completely visible in the window, fade it it */
+			if( bottom_of_window > bottom_of_object ){
+
+				$(this).addClass('slide_top')
+
+			}
+
+		}); 
+
+	});
+
+});
+
+$(document).ready(function() {
+
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+
+		/* Check the location of each desired element */
+		$('.slide_to_bot').each( function(i){
+
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			/* If the object is completely visible in the window, fade it it */
+			if( bottom_of_window > bottom_of_object ){
+
+				$(this).addClass('slide_bot')
+
+			}
+
+		}); 
+
+	});
+
+});
+
+
+
+
 $(document).ready(function () {
 
 	//row_hover
