b198f2471d9d4281b45ebac0c4c3f19d7a6c85cc
vitold <vditold@yandex.ru>
2016-11-14 15:14:27 +0300

    123

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 		/* Check the location of each desired element */
 		$('.hideme').each( function(i){
 
-			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_object = $(this).position().top + $(this).outerHeight() -30;
 			var bottom_of_window = $(window).scrollTop() + $(window).height();
 			/* If the object is completely visible in the window, fade it it */
 			if( bottom_of_window > bottom_of_object ){
