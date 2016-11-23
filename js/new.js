7e52127e705cf71c178b8dc8076e93976a8d3f22
vitold <vditold@yandex.ru>
2016-11-23 11:28:35 +0300

    123

--- js/new.js
+++ js/new.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 ; (function($, window, document, undefined) {
     var pluginName = "jqueryAccordionMenu";
     var defaults = {
-        speed: 300,
+        speed: 600,
         showDelay: 0,
         hideDelay: 0,
         singleOpen: true,
@@ -103,36 +103,4 @@ $(document).ready(function() {
 			$(this).addClass("active");
 		})	
 	});	
-	//поисковая строка
-	(function($) {
-		$.expr[":"].Contains = function(a, i, m) {
-			return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
-		};
-		function filterList(header, list) {
-			var form = $("<form>").attr({
-				"class":"filterform",
-				action:"#"
-			}), input = $("<input>").attr({
-				"class":"filterinput",
-				type:"text"
-			});
-			$(form).append(input).appendTo(header);
-			$(input).change(function() {
-				var filter = $(this).val();
-				if (filter) {
-					$matches = $(list).find("a:Contains(" + filter + ")").parent();
-					$("li", list).not($matches).slideUp();
-					$matches.slideDown();
-				} else {
-					$(list).find("li").slideDown();
-				}
-				return false;
-			}).keyup(function() {
-				$(this).change();
-			});
-		}
-		$(function() {
-			filterList($("#form"), $(".demo-list"));
-		});
-	})(jQuery);
 });
