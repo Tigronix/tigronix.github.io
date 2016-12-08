18d0b34463feaca2077fba108fb3802698088641
vitold <vditold@yandex.ru>
2016-12-08 13:24:19 +0300

    123123

--- js/script.js
+++ js/script.js
@@ -1,48 +1,279 @@
-$(document).ready(function(){
-	//Слайдер на  главной
-	$('.main_slider').slick({
-		infinite: true,
-		slidesToShow: 3,
-		slidesToScroll: 2,
-		centerMode: true,
-		arrows: true,
-		centerPadding: 0
+
+$(document).ready(function() {
+	//плавные якоря
+	$("a[href*=#]").bind("click", function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 2500);
+		e.preventDefault();
+		return false;
 	});
-	
-	//Стили для селектов
-	(function($) {
-		$(function() {
 
-			$('.styler').styler();
+	//row_hover
+	$('.rooms__section_table > .row').hover(function () {
+		var pTop = $(this).position().top -1;
+		$('#float_hover').css('top', pTop).show();
+	});
 
+
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+
+		/* Check the location of each desired element */
+		$('.hideme').each( function(i){
+
+			var bottom_of_object = $(this).position().top;
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			/* If the object is completely visible in the window, fade it it */
+			if( bottom_of_window > bottom_of_object ){
+
+				$(this).addClass('fadeIn');
+
+			}
+
+		}); 
+
+	});
+
+});
+
+$(document).ready(function() {
+
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+
+		/* Check the location of each desired element */
+		$('.slide_to_left').each( function(i){
+
+			var bottom_of_object = $(this).position().top;
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			/* If the object is completely visible in the window, fade it it */
+			if( bottom_of_window > bottom_of_object ){
+
+				$(this).addClass('slide_left')
+
+			}
+
+		}); 
+
+	});
+
+});
+
+$(document).ready(function() {
+
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+
+		/* Check the location of each desired element */
+		$('.slide_to_right').each( function(i){
+
+			var bottom_of_object = $(this).position().top;
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			/* If the object is completely visible in the window, fade it it */
+			if( bottom_of_window > bottom_of_object ){
+
+				$(this).addClass('slide_right')
+
+			}
+
+		}); 
+
+	});
+
+});
+
+$(document).ready(function() {
+
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+
+		/* Check the location of each desired element */
+		$('.slide_to_top').each( function(i){
+
+			var bottom_of_object = $(this).position().top;
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			/* If the object is completely visible in the window, fade it it */
+			if( bottom_of_window > bottom_of_object ){
+
+				$(this).addClass('slide_top')
+
+			}
+
+		}); 
+
+	});
+
+});
+
+$(document).ready(function() {
+
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+
+		/* Check the location of each desired element */
+		$('.slide_to_bot').each( function(i){
+
+			var bottom_of_object = $(this).position().top;
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			/* If the object is completely visible in the window, fade it it */
+			if( bottom_of_window > bottom_of_object ){
+
+				$(this).addClass('slide_bot')
+
+			}
+
+		}); 
+
+	});
+
+});
+
+
+
+
+$(document).ready(function () {
+	//slide_slider
+	$('.slide_from').click(function(){
+		$('.slide_that').toggleClass('slide_lefts').toggleClass('flex_show');
+
+	});
+
+
+
+
+
+
+	//animation
+	$('.room__about').addClass('slide_top');
+	$('.room__info').addClass('slide_bot');
+	//slider_hover
+	$('.owl-prev').hover(function () {
+		if ($(this).hover) {
+			$('.fa-angle-left').toggleClass('angle_hover');
+		}
+	});
+	$('.owl-next').hover(function () {
+		if ($(this).hover) {
+			$('.fa-angle-right').toggleClass('angle_hover');
+		}
+	});
+
+	//header-scroll
+	var header = $('.header__main');
+	$(window).scroll(function () {
+		var top = $(this).scrollTop();
+		if (top > 10) {
+			header.addClass('header__small');
+		} else {
+			header.removeClass('header__small');
+		}
+	});
+
+	//mob-menu
+	/*$('.header__open').click(function () {
+		$(this).fadeOut(300);
+		$('.header__close').fadeIn(300).addClass('ib');
+		$('.header__nav').addClass('header__on').removeClass('move_away').addClass('move_left_header').fadeIn(400);
+		$('.blackout').fadeIn(400);
+	});
+	$('.header__close').click(function () {
+		$(this).fadeOut(300);
+		$('.header__nav').removeClass('move_left_header').addClass('move_away').fadeOut(400);
+		$('.header__open').fadeIn(300).addClass('ib');
+		$('.blackout').fadeOut(400);
+	});*/
+	//mob-menu
+	$('.header__open').click(function () {
+		$(this).hide();
+		$('.header__close').show();	$('.room__menu').addClass('header__on').removeClass('move_away').addClass('move_left_header').fadeIn(400);	$('.mob_phone2').addClass('header__on').removeClass('move_away').addClass('move_left_header').fadeIn(400);
+		$('.header').addClass('z_index');
+		$('.blackout').fadeIn(400);
+	});
+	$('.header__close').click(function () {
+		$(this).hide();	$('.room__menu').removeClass('move_left_header').addClass('move_away').fadeOut(400);	$('.mob_phone2').removeClass('move_left_header').addClass('move_away').fadeOut(400);	
+		$('.header__open').show();
+		$('.blackout').fadeOut(400);
+		$('.header').removeClass('z_index');
+	});
+	$('.blackout').click(function(){
+		$(this).fadeOut(400);
+		$('.header__close').fadeOut(300);
+		$('.header__open').fadeIn(300);	$('.room__menu').removeClass('move_left_header').addClass('move_away').fadeOut(400);		$('.mob_phone2').removeClass('move_left_header').addClass('move_away').fadeOut(400);	
+		$('.header').removeClass('z_index');
+	});
+	//fancbyox
+	$(".fancybox").fancybox();
+
+
+	//rooms_slider
+	$(".rooms_slider").owlCarousel({
+		autoPlay: 5000,
+		stopOnHover: true,
+		navigation: true,
+		paginationSpeed: 1000,
+		goToFirstSpeed: 2000,
+		singleItem: true,
+		transitionStyle: "fade",
+		responsive: true,
+		autoHeight: true
+	});
+
+	//gal__slider
+	$(".gal__slider").owlCarousel({
+		autoPlay: 5000,
+		stopOnHover: true,
+		navigation: true,
+		paginationSpeed: 1000,
+		goToFirstSpeed: 2000,
+		singleItem: true,
+		transitionStyle: "fade",
+		responsive: true,
+	});
+
+
+
+	//rooms_slider
+	$(".pres__slider").owlCarousel({
+		autoPlay: 5000,
+		stopOnHover: true,
+		navigation: true,
+		paginationSpeed: 1000,
+		goToFirstSpeed: 2000,
+		singleItem: true,
+		transitionStyle: "fade",
+		responsive: true
+	});
+
+
+
+	//main_slider
+	$(".main_slider").owlCarousel({
+		autoPlay: 5000,
+		stopOnHover: true,
+		navigation: true,
+		paginationSpeed: 1000,
+		goToFirstSpeed: 2000,
+		singleItem: true,
+		transitionStyle: "fade",
+		responsive: true,
+		pagination: false
+	});
+
+});
+$(document).ready(function () {
+	//Universal Tabs
+	$(document).on('click', '[data-id]', function () {
+		var id = $(this).attr('data-id');
+		var taba = $(this).attr('data-taba');
+
+		$('[data-id="' + id + '"]').each(function () {
+			if ($(this).attr('data-taba') == taba) {
+				$(this).addClass('act').siblings().removeClass('act');
+			}
 		});
-	})(jQuery);
-	
-	//Открытие формы регистрации
-	$(".reg__open").fancybox({
-		width: 676,
-		height: 374,
-		maxWidth	: 800,
-		maxHeight	: 600,
-		fitToView	: false,
-		autoSize	: false,
-		closeClick	: false,
-		openEffect	: 'true',
-		closeEffect	: 'true'
+
+		$('#' + id + ' > [data-taba="' + taba + '"]').show().addClass('flex').siblings().hide().removeClass('flex');
 	});
-	
-	//Открытие формы обратной связи
-	$(".feedback__open").fancybox({
-		width: 676,
-		height: 374,
-		maxWidth	: 800,
-		maxHeight	: 600,
-		fitToView	: false,
-		autoSize	: false,
-		closeClick	: false,
-		openEffect	: 'true',
-		closeEffect	: 'true'
-	});
-	
-	
 });
---
+++ mail.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (isset($_POST['name'])) {$name = $_POST['name'];}
+	if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+	if (isset($_POST['message'])) {$message = $_POST['message'];}
+
+	$to = "scooter5@mail.ru";
+	$sendfrom   = "Заявка_с_сайта_Гермес";
+	$headers  = "From: " . strip_tags($sendfrom) . "\r\n";
+	$headers .= "Reply-To: ". strip_tags($sendfrom) . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+	$subject = "$formData";
+	$message = "$formData
+		<p><b>Имя:</b> $name</p>
+		<p><b>Телефон:</b> $phone</p>
+		<p><b>Сообщение:</b> $message</p>
+	";
+	$send = mail ($to, $sendfrom, $message, $headers);
+	if ($send == 'true')
+	{
+		echo '<div class="success"></div>';
+	}
+	else
+	{
+		echo '<div class="false"></div>';
+	}
+}?>
