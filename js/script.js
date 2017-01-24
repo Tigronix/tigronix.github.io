805df3abc9fa1aa9257e2c2a2b574e0657f4c48b
Vitold <vditold@yandex.ru>
2017-01-24 18:21:53 +0300

    123

--- js/script.js
+++ js/script.js
@@ -1,41 +1,77 @@
 $(document).ready(function(){
-  $('.photo').owlCarousel({
-        animateOut: 'fadeOut', 
-        animateIn: 'zoomOut', 
+  //photo_slider
+  $('.photo_slider').owlCarousel({
         items: 4, 
         autoplay: true,
         autoplayHoverPause: true, 
-        autoplayTimeout: 3000,
+        smartSpeed: 1000,
         nav: true,
         loop: true,
         margin: 36,
         responsive: {
             0 : {
-               nav: false
+               items: 1,
             },
-            769: {
-                nav: true
+            480 : {
+               items: 2,
+            },
+            769 : {
+               items: 3,
+            },
+            1025: {
+                items: 4,
             }
         }
      });
-    $('.slider_main').owlCarousel({
+    //slider-main
+    var owl = $('.slider-main');
+        owl.owlCarousel({
         animateOut: 'fadeOut', 
         animateIn: 'zoomOut', 
         items: 1, 
-        autoplay: true,
+       // autoplay: true,
         autoplayHoverPause: true, 
         autoplayTimeout: 3000,
         nav: true,
         loop: true,
-        margin: 36,
-        responsive: {
-            0 : {
-               nav: false
-            },
-            769: {
-                nav: true
-            }
-        }
+        
      });
+    // Listen to owl events:
+    owl.on('changed.owl.carousel', function(event) {
+        console.log(event.page.index);
+        $('.slider-nav_item:eq('+event.page.index+')').addClass('active-slide').siblings().removeClass('active-slide');
+    })
     
+    //Мобильное меню
+    $('.header__open').click(function () {
+        $(this).hide();
+        $('.header__close').show();
+        $('.header__nav').addClass('bounceInDownn').fadeIn(400);
+        $('.mob_phone2').addClass('bounceInDownn').addClass('header__on').fadeIn();
+        $('.blackout').fadeIn(400);
+        $('.global_wrapper').addClass('body_overflow');
+        $('.header_phone').fadeOut(400);
+    });
+    $('.header__close').click(function () {
+        $(this).hide
+        $('.header__nav').removeClass('bounceInDownn').fadeOut(400);
+        $('.mob_phone2').removeClass('bounceInDownn').fadeOut(400);
+        $('.header__open').fadeIn(400);
+        $('.header__close').fadeOut(400);
+        $('.blackout').fadeOut(400);
+        $('.global_wrapper').removeClass('body_overflow');
+        $('.header_phone').fadeIn(400);
+    });
+    $('.blackout').click(function () {
+        $(this).fadeOut(400);
+        $('.header__close').removeClass('bounceInDownn').fadeOut(400);
+        $('.header__open').removeClass('bounceInDownn').fadeIn(400);
+        $('.header__nav').fadeOut(400);
+        $('.mob_phone2').fadeOut(400);
+        $('.global_wrapper').removeClass('body_overflow');
+        $('.header_phone').fadeIn(400);
+    });
+    
+    //fancybox
+    $(".fancybox").fancybox();     
 });
