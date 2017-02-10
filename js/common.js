73c96175840145990a1380886c54035d86b848af
Vitold <vditold@yandex.ru>
2017-02-10 13:20:27 +0300

    123

---
+++ js/common.js
@@ -0,0 +1,31 @@
+$(function() {
+		//Universal Tabs
+    $(document).on('click', '[data-id]', function () {
+        var id = $(this).attr('data-id');
+        var taba = $(this).attr('data-taba');
+        $('[data-id="' + id + '"]').each(function () {
+            if ($(this).attr('data-taba') == taba) {
+                $(this).addClass('act').siblings().removeClass('act');
+            }
+        });
+        $('#' + id + ' > [data-taba="' + taba + '"]').show().addClass('flex').siblings().hide().removeClass('flex');
+    });
+	
+		//slick
+		$('.constructor__combo-slider').slick({
+			infinite: true,
+			slidesToShow: 5,
+			slidesToScroll: 1,
+			arrows: true
+		});
+	
+		
+	
+	//styler
+	(function ($) {
+		$(function () {
+			$('.styler').styler();
+		});
+	})(jQuery);
+
+});
