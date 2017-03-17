bea0a1cc0fc21c252f76216abe4de07480e9fe8c
Vitold Demishkevich <vditold@yandex.ru>
2017-03-17 18:06:37 +0300

    zsa(half)

--- js/common.js
+++ js/common.js
@@ -1,23 +1,33 @@
 $(document).ready(function(){
+	//header-scroll
+    var header = $('.page-header');
+    $(window).scroll(function () {
+        var top = $(this).scrollTop();
+        if (top > 50) {
+            header.addClass('page-header--fixed');
+        }
+        else {
+            header.removeClass('page-header--fixed');
+        }
+    });
 	 //mob-menu-плавное появление
     $('.header__open').click(function () {
         $(this).hide();
         $('.header__close').show();
-        $('.room__nav').show();
-        $('.side__menu').addClass('bounceInDownn').show().fadeIn();
+        $('.nav').addClass('bounceInDownn').show().fadeIn();
         $('.mob_phone2').addClass('bounceInDownn').addClass('header__on').fadeIn();
-        $('.header').addClass('z_index');
+        $('.page-header').addClass('z-index');
         $('.blackout').fadeIn(400);
         $('.global_wrapper').addClass('body_overflow');
     });
     $('.header__close').click(function () {
         $(this).hide();
         $('.room__nav').fadeOut();
-        $('.side__menu').removeClass('bounceInDownn').fadeOut();
+        $('.nav').removeClass('bounceInDownn').fadeOut();
         $('.mob_phone2').removeClass('bounceInDownn').fadeOut();
         $('.header__open').show();
         $('.blackout').fadeOut(400);
-        $('.header').removeClass('z_index');
+        $('.page-header').removeClass('z-index');
         $('.global_wrapper').removeClass('body_overflow');
     });
     $('.blackout').click(function () {
@@ -25,9 +35,9 @@ $(document).ready(function(){
         $('.header__close').removeClass('bounceInDownn').fadeOut();
         $('.header__open').removeClass('bounceInDownn').fadeIn();
         $('.room__nav').fadeOut(400);
-        $('.side__menu').fadeOut(400);
+        $('.nav').fadeOut(400);
         $('.mob_phone2').fadeOut(400);
-        $('.header').removeClass('z_index');
+        $('.page-header').removeClass('z-index');
         $('.global_wrapper').removeClass('body_overflow');
     });
 });
