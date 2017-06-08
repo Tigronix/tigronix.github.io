72f9a7d1f3b7fa6634e47784a9ee358c7f97478e
Vitold Demishkevich <vditold@yandex.ru>
2017-06-08 18:47:35 +0300

    123

--- js/common.js
+++ js/common.js
@@ -77,7 +77,7 @@ $(document).ready(function () {
 		var w_height = $(window).height(); // Высота окна браузера
 		var d_height = $(document).height(); // Высота всего документа
 		var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-		if (w_top + 200 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+		if (w_top + 400 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
 			$(".spincrement").spincrement({
 				thousandSeparator: ""
 				, duration: 1200
