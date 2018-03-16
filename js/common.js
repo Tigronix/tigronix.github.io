881676eecbb3e586455e7ba610ec4621783a0511
Tigronix <vditold@yandex.ru>
2018-03-16 17:04:18 +0300

    123

---
+++ js/common.js
@@ -0,0 +1,465 @@
+"use strict"
+//templates
+new Vue({
+		el: '#global-svg',
+		template: '<svg aria-hidden="true" style="position: absolute; width: 0; height: 0; overflow: hidden;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="icon-keyboard-arrow-left" viewBox="0 0 32 32"><title>keyboard-arrow-left</title><path d="M20.547 21.453l-6.107-6.12 6.107-6.12-1.88-1.88-8 8 8 8z"></path></symbol><symbol id="icon-balance-scale" viewBox="0 0 40 32"><title>balance-scale</title><path d="M22 28h10.5c0.828 0 1.5 0.672 1.5 1.5v1c0 0.828-0.672 1.5-1.5 1.5h-25c-0.828 0-1.5-0.672-1.5-1.5v-1c0-0.828 0.672-1.5 1.5-1.5h10.5v-18.417c-1.471-0.643-2.573-1.972-2.9-3.583h-7.6c-0.828 0-1.5-0.672-1.5-1.5v-1c0-0.828 0.672-1.5 1.5-1.5h8.5c0.912-1.214 2.364-2 4-2s3.088 0.786 4 2h8.5c0.828 0 1.5 0.672 1.5 1.5v1c0 0.828-0.672 1.5-1.5 1.5h-7.6c-0.327 1.611-1.429 2.94-2.9 3.583v18.417zM39.999 21c0-1.011 0.084-0.545-5.316-11.344-1.103-2.206-4.262-2.21-5.367 0-5.445 10.891-5.316 10.365-5.316 11.344h-0.001c0 2.761 3.582 5 8 5s8-2.239 8-5h-0.001zM32 11l4.5 9h-9l4.5-9zM15.999 21c0-1.011 0.084-0.545-5.316-11.344-1.103-2.206-4.262-2.21-5.367 0-5.446 10.891-5.316 10.365-5.316 11.344h-0.001c0 2.761 3.582 5 8 5s8-2.239 8-5h-0.001zM8 11l4.5 9h-9l4.5-9z"></path></symbol><symbol id="icon-ruble-sign" viewBox="0 0 24 32"><title>ruble-sign</title><path d="M14.96 20c5.32 0 9.040-3.716 9.040-9.058s-3.72-8.942-9.040-8.942h-10.21c-0.414 0-0.75 0.336-0.75 0.75v12.915h-3.25c-0.414 0-0.75 0.336-0.75 0.75v2.835c0 0.414 0.336 0.75 0.75 0.75h3.25v2h-3.25c-0.414 0-0.75 0.336-0.75 0.75v2.5c0 0.414 0.336 0.75 0.75 0.75h3.25v3.25c0 0.414 0.336 0.75 0.75 0.75h3.66c0.414 0 0.75-0.336 0.75-0.75v-3.25h10.090c0.414 0 0.75-0.336 0.75-0.75v-2.5c0-0.414-0.336-0.75-0.75-0.75h-10.090v-2h5.8zM9.16 6.297h4.92c2.92 0 4.68 1.819 4.68 4.645 0 2.864-1.76 4.723-4.76 4.723h-4.84v-9.368z"></path></symbol><symbol id="icon-keyboard-arrow-right" viewBox="0 0 32 32"><title>keyboard-arrow-right</title><path d="M11.453 21.787l6.107-6.12-6.107-6.12 1.88-1.88 8 8-8 8z"></path></symbol><symbol id="icon-access-alarm" viewBox="0 0 32 32"><title>access-alarm</title><path d="M29.333 7.627l-6.133-5.147-1.72 2.040 6.133 5.147 1.72-2.040zM10.507 4.52l-1.707-2.040-6.133 5.133 1.72 2.040 6.12-5.133zM16.667 10.667h-2v8l6.333 3.8 1-1.64-5.333-3.16v-7zM16 5.333c-6.627 0-12 5.373-12 12s5.36 12 12 12c6.627 0 12-5.373 12-12s-5.373-12-12-12zM16 26.667c-5.16 0-9.333-4.173-9.333-9.333s4.173-9.333 9.333-9.333 9.333 4.173 9.333 9.333-4.173 9.333-9.333 9.333z"></path></symbol><symbol id="icon-shopping-cart" viewBox="0 0 32 32"><title>shopping-cart</title><path d="M9.333 24c-1.467 0-2.653 1.2-2.653 2.667s1.187 2.667 2.653 2.667 2.667-1.2 2.667-2.667-1.2-2.667-2.667-2.667zM1.333 2.667v2.667h2.667l4.8 10.12-1.8 3.267c-0.213 0.373-0.333 0.813-0.333 1.28 0 1.467 1.2 2.667 2.667 2.667h16v-2.667h-15.44c-0.187 0-0.333-0.147-0.333-0.333l0.040-0.16 1.2-2.173h9.933c1 0 1.88-0.547 2.333-1.373l4.773-8.653c0.107-0.187 0.16-0.413 0.16-0.64 0-0.733-0.6-1.333-1.333-1.333h-19.72l-1.253-2.667h-4.36zM22.667 24c-1.467 0-2.653 1.2-2.653 2.667s1.187 2.667 2.653 2.667 2.667-1.2 2.667-2.667-1.2-2.667-2.667-2.667z"></path></symbol><symbol id="icon-track-changes" viewBox="0 0 32 32"><title>track-changes</title><path d="M25.427 6.573l-1.88 1.88c1.92 1.933 3.12 4.6 3.12 7.547 0 5.893-4.773 10.667-10.667 10.667s-10.667-4.773-10.667-10.667c0-5.44 4.067-9.92 9.333-10.573v2.693c-3.787 0.64-6.667 3.92-6.667 7.88 0 4.413 3.587 8 8 8s8-3.587 8-8c0-2.213-0.893-4.213-2.347-5.653l-1.88 1.88c0.96 0.973 1.56 2.307 1.56 3.773 0 2.947-2.387 5.333-5.333 5.333s-5.333-2.387-5.333-5.333c0-2.48 1.707-4.547 4-5.147v2.853c-0.8 0.467-1.333 1.307-1.333 2.293 0 1.467 1.2 2.667 2.667 2.667s2.667-1.2 2.667-2.667c0-0.987-0.533-1.84-1.333-2.293v-11.040h-1.333c-7.36 0-13.333 5.973-13.333 13.333s5.973 13.333 13.333 13.333 13.333-5.973 13.333-13.333c0-3.68-1.493-7.013-3.907-9.427z"></path></symbol></defs></svg>'
+	})
+
+function scrollEffects() {
+	new WOW().init();
+}
+
+//функция помощник: siblings
+var getSiblings = function(elem) {
+	var siblings = [];
+	var sibling = elem.parentNode.firstChild;
+	for(; sibling; sibling = sibling.nextSibling) {
+		if(sibling.nodeType !== 1 || sibling === elem) continue;
+		siblings.push(sibling);
+	}
+	return siblings;
+}
+
+//active
+function active(elem) {
+	$(elem).on('click', function(){
+		if($(this).hasClass('active')){
+			$(this).removeClass('active');
+		}else{
+			$(this).addClass('active');
+		}
+	});
+}
+
+//active & siblings off
+function activeSiblings(elem) {
+	$(elem).on('click', function(){
+		$(this).addClass('active').siblings().removeClass('active');
+	});
+}
+
+//active other
+function activerOther(elem, target) {
+	$(elem).on('click', function(){
+		$(target).removeClass('menu__hide').toggleClass('active');
+	});
+}
+
+//bodyOverflow
+function bodyOverflow(elem) {
+	var btn = $(elem);
+	var body = $('body');
+	btn.on('click', function(){
+		body.toggleClass('overflow');
+	});
+}
+
+//toggleClass on window scroll
+function changeClassOnScroll(){
+	$(window).scroll(function() {
+    var elem = $('.page-header');
+    var height = elem.innerHeight();
+    var top = $(this).scrollTop();
+		var pageTop = $('.page-header__top');
+		var pageBot = $('.page-header__bottom');
+		var windowWidth = $(window).width();
+
+    if (top > 50) {
+			$(elem).addClass('scroll');
+      $(pageTop).hide();
+			$(pageBot).addClass('scroll');
+    } else {
+			$(elem).removeClass('scroll');
+      $(pageTop).show();
+			$(pageBot).removeClass('scroll');
+    }
+
+		if(windowWidth <= 1024){
+			if (top > 50) {
+				$(elem).removeClass('scroll');
+	      $(pageTop).show();
+				$(pageBot).removeClass('scroll');
+	    } 
+		}
+  });
+}
+
+//Universal Tabs
+function tabs() {
+	$(document).on('click', '[data-tabclass]', function() {
+		var content = $(this).attr('data-tabclass');
+		var number = $(this).attr('data-tabnumber');
+		$('[data-tabclass="' + content + '"]').each(function() {
+			if ($(this).attr('data-tabnumber') == number) {
+				$(this).addClass('active').siblings().removeClass('active');
+			}
+		});
+		$('.' + content + ' > [data-tabnumber="' + number + '"]').show().addClass('active flex animated').removeClass('hide').css('display', 'flex').siblings().hide().removeClass('active flex animated showFlex');
+	});
+};
+
+//animate
+function animate(button, content, animationIn, animationOut, hideTiming) {
+	hideTiming = hideTiming || 1000;
+	var elem = document.querySelector(content);
+	$(button).on('click', function() {
+		$(content).show();
+		var animateContent = content;
+		if ($(content).hasClass(animationIn)) {
+			$(content).removeClass(animationIn).addClass(animationOut + ' animated');
+		} else {
+			$(content).removeClass(animationOut).addClass(animationIn + ' animated');
+		}
+	});
+}
+
+//animate + hide
+function animateHide(button, content, animationIn, animationOut, hideTiming) {
+	hideTiming = hideTiming || 1000;
+	var elem = document.querySelector(content);
+	$(button).on('click', function() {
+		$(content).show();
+		var animateContent = content;
+		if ($(content).hasClass(animationIn)) {
+			$(content).removeClass(animationIn).addClass(animationOut + ' animated');
+			setTimeout(function(){e
+				$(content).hide();
+			}, hideTiming);
+		} else {
+			$(content).removeClass(animationOut).addClass(animationIn + ' animated');
+		}
+	});
+}
+
+//owl-slider
+function slider(animationIn, animationOut, elem, items, navContainer, navText) {
+	animationIn = animationIn || 'zoomIn';
+	animationOut = animationOut || 'fadeOut';
+	elem = elem || '.popular__slider';
+	items = items || 5;
+	navContainer = navContainer || '';
+	navText = navText || ['<svg class="icon icon-keyboard-arrow-left"><use xlink:href="#icon-keyboard-arrow-left"></use></svg>', '<svg class="icon icon-keyboard-arrow-right"><use xlink:href="#icon-keyboard-arrow-right"></use></svg>'];
+	if(elem){
+		$(elem).owlCarousel({
+			nav: true,
+			navContainer: navContainer,
+			navText: navText,
+			items: items,
+			animateIn: animationIn,
+			animateOut: animationOut,
+			loop: true,
+			URLhashListener: true,
+      startPosition: 'URLHash',
+			dots: true,
+			dotsEach: true,
+			dotsContainer: '.popular__dots',
+			responsive: {
+				0: {
+					items: 1
+				},
+				500: {
+					items: 2
+				},
+				768: {
+					items: 3
+				},
+				1025: {
+					items: 5
+				},
+			}
+		});
+	}
+}
+function carousel(animationIn, animationOut, elem, items, navContainer, navText) {
+	animationIn = animationIn || 'zoomIn';
+	animationOut = animationOut || 'fadeOut';
+	elem = elem || '.pies__slider';
+	items = items || 4;
+	navContainer = navContainer || '';
+	navText = navText || ['<svg class="icon icon-keyboard-arrow-left"><use xlink:href="#icon-keyboard-arrow-left"></use></svg>', '<svg class="icon icon-keyboard-arrow-right"><use xlink:href="#icon-keyboard-arrow-right"></use></svg>'];
+	if(elem){
+		$(elem).owlCarousel({
+			nav: true,
+			navContainer: navContainer,
+			navText: navText,
+			items: items,
+			animateIn: animationIn,
+			animateOut: animationOut,
+			loop: true,
+			URLhashListener: true,
+      startPosition: 'URLHash',
+			dots: true,
+			dotsEach: true,
+			dotsContainer: '.pies__dots',
+			responsive: {
+				0: {
+					items: 1
+				},
+				600: {
+					items: 2
+				},
+				768: {
+					items: 3
+				},
+				1025: {
+					items: 4
+				},
+			}
+		});
+	}
+}
+
+//spincrement
+function spincrement() {
+	var elem = $('.spincrement');
+	if(elem){
+		$(".spincrement").spincrement();
+		var show = true;
+		var countbox = ".stats";
+		$(window).on("scroll load resize", function () {
+			if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+			var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+			var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+			var w_height = $(window).height(); // Высота окна браузера
+			var d_height = $(document).height(); // Высота всего документа
+			var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+			if (w_top + 400 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+				$(".spincrement").spincrement({
+					thousandSeparator: ""
+					, duration: 3000
+				});
+				show = false;
+			}
+		});
+	}
+}
+
+//Маска для телефона
+function phoneMask() {
+	if($('*').is('.formTel')){
+		$(".formTel").mask("+7(999)999-99-99", {
+			placeholder: "+7(___)___-__-__"
+		});
+	}
+}
+
+//tabsNav
+function tabsNav() {
+	var containers = document.querySelectorAll('.choose__roomNav-wrap');
+	containers.forEach(function(item, index, array){
+		var prev = item.querySelector('.choose__roomNav-btn--prev');
+		var next = item.querySelector('.choose__roomNav-btn--next');
+		var navItems = item.querySelector('.choose__roomNav').childNodes;
+		var navitemFirst = navItems[1];
+		var navitemLast = navItems[7];
+
+		function tabsPrev() {
+			var navItemActive = item.querySelector('.choose__roomNav-item.active');
+			var navItemPrev = navItemActive.previousElementSibling;
+
+			navItemActive.classList.remove('active');
+			if(navItemPrev){
+				var itemDataClass = navItemPrev.getAttribute('data-tabclass');
+				var itemDataNumber = navItemPrev.getAttribute('data-tabnumber');
+				var tabContent = item.nextElementSibling.querySelectorAll('.choose__room-item');
+
+				tabContent.forEach(function(item, index, array) {
+					var itemContentNumber = item.getAttribute('data-tabnumber');
+					item.style.display = 'none';
+
+					if(itemContentNumber == itemDataNumber){
+						item.style.display = 'block';
+						item.classList.add('animated');
+					}
+				});
+
+				navItemPrev.classList.add('active');
+			}else{
+				navitemFirst.classList.add('active');
+			}
+		}
+		function tabsNext() {
+			var navItemActive = item.querySelector('.choose__roomNav-item.active');
+			var navItemNext = navItemActive.nextElementSibling;
+
+			navItemActive.classList.remove('active');
+			if(navItemNext){
+				var itemDataClass = navItemNext.getAttribute('data-tabclass');
+				var itemDataNumber = navItemNext.getAttribute('data-tabnumber');
+
+				var tabContent = item.nextElementSibling.querySelectorAll('.choose__room-item');
+				tabContent.forEach(function(item, index, array) {
+					var itemContentNumber = item.getAttribute('data-tabnumber');
+					item.style.display = 'none';
+
+					if(itemContentNumber == itemDataNumber){
+						item.style.display = 'block';
+						item.classList.add('animated');
+					}
+				});
+
+				navItemNext.classList.add('active');
+			}else{
+				navitemLast.classList.add('active');
+			}
+		}
+
+		prev.addEventListener('click', tabsPrev);
+		next.addEventListener('click', tabsNext);
+	});
+}
+
+//resizeFix
+function resizeFix(elem) {
+	$(window).on('resize', function(){
+		var windowWidth = $(window).width();
+		if(windowWidth > 1024){
+			$(elem).show().removeClass('animated').css('display', 'flex');
+		}else {
+			$(elem).addClass('animated').css('display', 'none');
+			$('.hamburger').removeClass('active');
+			$('.hamburger__wrap').removeClass('active');
+			$('.hamburger__line1').removeClass('rotate_in_45');
+			$('.hamburger__line3').removeClass('rotate_in_-45');
+			$('.menu').removeClass('animated, slideInLeft');
+			$('body').removeClass('overflow');
+		}
+	});
+}
+
+//productToggle
+function productToggle(elem) {
+	$(elem).on('click', function(){
+		var target = $(this).closest('.product__buy').find('.product__calc');
+
+		$(this).addClass('animated fadeOut');
+		$(target).show().addClass('animated slideInUp');
+	});
+}
+
+//catalogCalc
+function catalogCalc(){
+	var select = $('.calcSelect');
+	var btn = $('.calcBtn');
+	var dec = $('.calcDec');
+	var inc = $('.calcInc');
+	var output = $('.calcNumber');
+	var outputText = '.calcNumber';
+	var valueBox = $('.calcOutput');
+	var valueBoxText = '.calcOutput';
+	var submit = $('.calcSubmit');
+	var singleItem = $('.calcSingleItem');
+
+	if(select){
+		$(select).on('change', function(){
+		var options = $(this).find('option');
+		var value = $(this).val();
+		var output = $(this).closest('.product__item').find(outputText);
+		var valueBox = $(this).closest('.product__item').find(valueBoxText);
+		console.log(valueBox);
+		options.each(function(){
+			var optionValue = $(this).val();
+			if(value == optionValue){
+				var price = $(this).data('price');
+				$(output).html(price).attr('data-price', price);
+				$(valueBox).html(1);
+			}
+		});
+	});
+	}
+
+	if(btn){
+		$(btn).on('click', function(){
+		var output = $(this).closest('.product__item').find(outputText);
+		var value = $(this).closest('.product__item').find(valueBoxText);
+		var valueHtml = $(value).html();
+
+		if($(this).hasClass('calcDec')){
+			--valueHtml
+		}else if($(this).hasClass('calcInc')){
+			++valueHtml
+		}
+		if(valueHtml <= 1){
+			valueHtml = 1;
+		}
+		var summ = valueHtml * parseInt(output.attr('data-price'));
+
+		$(value).html(valueHtml);
+		$(output).html(summ);
+	});
+	}
+
+	if(submit){
+		$(submit).on('click', function(){
+			var output = $(this).closest('.product__item').find(outputText);
+			var value = $(this).closest('.product__item').find(valueBoxText);
+			var cart = $('.calcCart');
+			var cartCount = $('.calcCartCount');
+
+			var summ = parseInt(cart.html()) + parseInt(output.html());
+			var valueSumm = parseInt(value.html()) + parseInt(cartCount.html());
+
+			cartCount.html(valueSumm);
+			cart.html(summ);
+		});
+	}
+
+	if(singleItem){
+		$(singleItem).on('click', function(){
+			var output = $(this).closest('.popular__item').find(outputText);
+			var cart = $('.calcCart');
+			var cartCount = $('.calcCartCount');
+
+			var summ = parseInt(cart.html()) + parseInt(output.html());
+			var valueSumm = 1 + parseInt(cartCount.html());
+
+			cartCount.html(valueSumm);
+			cart.html(summ);
+		});
+	}
+}
+
+function headerMargin() {
+	var headerHeight = $('.page-header').innerHeight();
+	var main = $('.main');
+
+	main.css('marginTop', headerHeight + 30);
+}
+
+
+window.onload = function() {
+	//scrollEvents
+	scrollEffects();
+	changeClassOnScroll();
+
+	//other
+	bodyOverflow('.hamburger');
+	phoneMask();
+	headerMargin();
+	resizeFix('.menu');
+
+	//calc
+	catalogCalc();
+
+	//tabs
+	tabs();
+
+	//active toggle
+	active('.hamburger');
+	active('.hamburger__wrap');
+
+	//activeSiblings
+
+	//activeOther
+
+
+	//Animation
+	animate('.hamburger', '.hamburger__line1', 'rotate_in_45', 'rotate_in_45_out');
+	animate('.hamburger', '.hamburger__line3', 'rotate_in_-45', 'rotate_in_-45_out');
+	//Animation+hide
+	animate('.hamburger__wrap', '.menu', 'slideInLeft', 'slideOutLeft');
+
+	//slider(animationIn, animationOut, elem, items, navContainer, navText)
+	slider();
+	carousel();
+};
